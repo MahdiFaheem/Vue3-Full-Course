dc02724927193756aff20e50406e3d1981e703c3
MahdiFaheem <mahdihasanfaheem@gmail.com>
2020-11-20 14:01:07 +0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>DC Heros</h1>
+  <ul>
+    <li v-for="(dcHero,index) in dcHeros" :key="dcHero.name">{{++index}}) {{dcHero.name}}</li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{ 
+     dcHeros: [
+        {name: "SuperGirl"},
+        {name: "Flash"},
+        {name: "BatMan"},
+        {name: "Arrow"},
+        {name: "SuperMan"},
+      ],
+    };
+  },
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
